50a79df3ede9bb9a7c7d398f52c6d77f48481545
Rian <62795792+rian55555@users.noreply.github.com>
2022-06-28 10:12:58 +0300

    Connected to firebase

--- repApp.py
+++ repApp.py
@@ -2,7 +2,32 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
 from PyPDF2 import PdfReader
+import firebase_admin
+from firebase_admin import credentials
+from google.cloud import firestore
+from google.cloud import storage
+import os
 
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:/Users/90531/Documents/=EworldPartner/report-app-666-1d6bc3944d21.json"
+storage_client = storage.Client()
+cred = credentials.Certificate("C:/Users/90531/Documents/=EworldPartner/report-app-666-firebase-adminsdk-13k54-2f736e34e8.json")
+firebase_admin.initialize_app(cred)
+
+
+
+def get_check_exists():
+    db = firestore.Client()
+    # [START firestore_data_get_as_map]
+    doc_ref = db.collection(u'User').document(u'e4RuH26D0yo7iFKJrRpd')
+
+    doc = doc_ref.get()
+    if doc.exists:
+        print(f'Document data: {doc.to_dict()}')
+    else:
+        print(u'No such document!')
+
+get_check_exists()
 # Root window
 root = tk.Tk()
 root.title('RepApp')
@@ -11,11 +36,8 @@ root.geometry('550x250')
 
 def readPdf(filename):
     reader = PdfReader(open(filename, 'rb'))
-    page = reader.pages[0]
-    print(page.extract_text())
-    page = reader.pages[1]
-    print(page.extract_text())
-    filename.close()
+    for page in reader.pages:
+        print(page.extractText())
 
 
 def open_text_file():
