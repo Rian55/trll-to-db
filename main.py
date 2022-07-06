4a0ef2f4df4e6b51caac56730063764c48d2cb67
Rian <59274842+Rian55@users.noreply.github.com>
2022-07-06 16:36:36 +0300

    added write to logs

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ from firebase_admin import initialize_app
 from firebase_admin import credentials
 from google.cloud import firestore
 import os
+from datetime import datetime
 
+
+
+def toLog(text):
+    file = open('logs.txt', 'a+')
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(dt_string,": ",text, file = file)
+    file.close()
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = k.path_to_GAC
 CREDENTIALS = credentials.Certificate(k.path_to_certificate)
@@ -20,15 +29,23 @@ CLIENT = TrelloClient(
     #token_secret = 'your-oauth-token-secret'
 )
 
-organizations = CLIENT.list_organizations()
+organizations = []
+ALL_BOARDS = []
+ALL_MEMBERS = []
 
-EWP_ORGANIZATION = organizations[0]
-for org in organizations:
-    if org.name == "ewpteam":
-        EWP_ORGANIZATION = org
+try:
+    organizations = CLIENT.list_organizations()
+    EWP_ORGANIZATION = organizations[0]
 
-ALL_BOARDS = EWP_ORGANIZATION.get_boards(list_filter = "open")
-ALL_MEMBERS = EWP_ORGANIZATION.get_members()
+    for org in organizations:
+        if org.name == "ewpteam":
+            EWP_ORGANIZATION = org
+
+    ALL_BOARDS = EWP_ORGANIZATION.get_boards(list_filter = "open")
+    ALL_MEMBERS = EWP_ORGANIZATION.get_members()
+    toLog("Succesfully getting organizations/boards/members")
+except:
+    toLog("Error getting organizations/boards/members")
 
 def set_update_boards(boards):
     boards_to_send = []
@@ -43,13 +60,18 @@ def set_update_boards(boards):
         new_board = Board.Board(id = board.id, name = board.name, members = members, lists = lists)
         boards_to_send.append(new_board)
 
+    toLog("Board list has been succesfully created.")
     boards_ref = FIRESTORE.collection(u'boards')
 
     for board in boards_to_send:
-        print(board)
         boards_ref.document(board.id).set(board.to_dict(), merge = True)
 
-set_update_boards(ALL_BOARDS)
+    toLog("Board list has been succesfully pushed to firestore.")
+
+try:
+    set_update_boards(ALL_BOARDS)
+except:
+    toLog("Error in set_update_boards")
 
 
 def set_update_tasks(boards):
@@ -68,10 +90,15 @@ def set_update_tasks(boards):
                                            createdDate = task.created_date, board = board.name,\
                                            members = members, dueDate = task.due_date))
 
+    toLog("Task list has been succesfully created.")
     tasks_ref = FIRESTORE.collection(u'tasks')
 
     for task in tasks_to_send:
-        print(task)
         tasks_ref.document(task.id).set(task.to_dict(), merge = True)
 
-set_update_tasks(ALL_BOARDS)
+    toLog("Task list has been succesfully pushed to firestore.")
+
+try:
+    set_update_tasks(ALL_BOARDS)
+except:
+    toLog("Error in set_update_tasks.")
